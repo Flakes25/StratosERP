ec4393f32c4f353a7ce9fbccb75ef685d7abd5a9
Flakes25 <maveipecson.25@gmail.com>
2026-09-12 00:35:14 +0800

    Create stratoserp dashboard

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Resource, Infrastructure, HardwareNode
 
-# Create your views here.
+
+def dashboard(request):
+    resources = Resource.objects.count()
+    infrastructure = Infrastructure.objects.count()
+    hardware_nodes = HardwareNode.objects.count()
+
+    return render(request, "core/dashboard.html", {
+        "resources": resources,
+        "infrastructure": infrastructure,
+        "hardware_nodes": hardware_nodes,
+    })
